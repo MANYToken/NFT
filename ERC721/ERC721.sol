bea85740a18561ecf3112c181ac2774c90fdaff7
dogepreacher <36780059+dogepreacher@users.noreply.github.com>
2020-10-11 14:16:54 -0700

    test contract deployed to main net

--- ERC721/ERC721.sol
+++ ERC721/ERC721.sol
@@ -96,7 +96,7 @@ contract ERC721 is
 
     address internal constant MOAR = 0xBf131dCbE3436dab8a7c82D9C3666d652ca38eaB;
     address internal constant BURN = 0x0420420420420420420420420420420420420420;
-    IERC20 public ERCInterface;
+    //IERC20 public ERCInterface;
     uint256 private constant rare = 10000000000000000000; // 10 // costs to mint in moar
     uint256 private constant epic = 25000000000000000000; // 25
     uint256 private constant legendary = 137000000000000000000; // 137
@@ -108,7 +108,7 @@ contract ERC721 is
     constructor(string memory name, string memory symbol) public {
         _name = name;
         _symbol = symbol;
-        ERCInterface = IERC20(MOAR);
+        //ERCInterface = IERC20(MOAR);
 
         // register the supported interfaces to conform to ERC721 via ERC165
         _registerInterface(_INTERFACE_ID_ERC721);
@@ -606,9 +606,9 @@ contract ERC721 is
 
     function mintRare(string memory _tokenURI) public {
         address buyer = _msgSender();
-        ERCInterface = IERC20(MOAR);
-        ERCInterface.safeApprove(address(this), rare);
-        ERCInterface.safeTransferFrom(buyer, BURN, rare);
+        //ERCInterface = IERC20(MOAR);
+        //ERCInterface.safeApprove(address(this), rare);
+        //ERCInterface.safeTransferFrom(buyer, BURN, rare);
         uint256 newTokenId = _getNextTokenId().add(rare);
         _mint(buyer, newTokenId);
         _setTokenURI(newTokenId, _tokenURI);
@@ -617,9 +617,9 @@ contract ERC721 is
 
     function mintEpic(string memory _tokenURI) public {
         address buyer = _msgSender();
-        ERCInterface = IERC20(MOAR);
-        ERCInterface.safeApprove(address(this), epic);
-        ERCInterface.safeTransferFrom(buyer, BURN, epic);
+        //ERCInterface = IERC20(MOAR);
+        //ERCInterface.safeApprove(address(this), epic);
+        //ERCInterface.safeTransferFrom(buyer, BURN, epic);
         uint256 newTokenId = _getNextTokenId().add(epic);
         _mint(buyer, newTokenId);
         _setTokenURI(newTokenId, _tokenURI);
@@ -628,9 +628,9 @@ contract ERC721 is
 
     function mintLegendary(string memory _tokenURI) public {
         address buyer = _msgSender();
-        ERCInterface = IERC20(MOAR);
-        ERCInterface.safeApprove(address(this), legendary);
-        ERCInterface.safeTransferFrom(buyer, BURN, legendary);
+        //ERCInterface = IERC20(MOAR);
+        //ERCInterface.safeApprove(address(this), legendary);
+        //ERCInterface.safeTransferFrom(buyer, BURN, legendary);
         uint256 newTokenId = _getNextTokenId().add(legendary);
         _mint(buyer, newTokenId);
         _setTokenURI(newTokenId, _tokenURI);
--- readme.md
+++ readme.md
@@ -1,15 +1,21 @@
-Deployed to Main net at address
-0x4d32D2D14730A62eB1FDF96cF580C2024a631e84
+Deployed to Main net at address here
+https://etherscan.io/address/0x11ca23350f873e5352345ad4df5ab8259e97c6b0
+
+3 tokens have been created, 1 of each rarity
+https://etherscan.io/tx/0x7d0f2a8b2e67e646a5e34515bbeb4d3dbf84e195b35ba897570661622fb5f605
+https://etherscan.io/tx/0xb0ad45140a534482446676a79442da9470062a0977d7da7ab59231286b11de4c
+https://etherscan.io/tx/0x20e0c5ec450d290853539eac5846c4a5db716079b861b685b098773c762d56a4
+
+
 
 test version changes
-- burn to mint costs reduced to 1, 2, 3 from 10, 25, 137 (final costs tbd)
+- burn when create was removed and needs to be fixed
 
 
 
 next to do
 - add function to mint a a limited number of copies of already existing tokens
-- consider extra cost to transfer
-    - cost paid to owner or burned or split between both
+    - this will burn at least the same amount of tokens as the priginal mint, and also pay some to the creator
 
 
 
@@ -17,4 +23,3 @@ import ABI to front end to access functions
 
 Important function calls are "mintRare", "mintEpic", "mintlegendary" 
 - They take 1 arg, a string. This will be the address of picture to load
-- Call takes MOAR from your account and BURNS it - NOT TESTED YET 
